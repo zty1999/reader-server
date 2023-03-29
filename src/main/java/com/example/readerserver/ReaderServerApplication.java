b8e35bf00f7f30267cb465be458ca890baae4c8b
zty1999 <1685470954@qq.com>
2023-03-29 19:19:55 +0800

    项目搭建

---
+++ src/main/java/com/example/readerserver/ReaderServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.readerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReaderServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/readerserver/ReaderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
