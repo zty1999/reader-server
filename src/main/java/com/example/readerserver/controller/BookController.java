3cad575f9fb48abb41d496517a7429c2b917900a
zty1999 <1685470954@qq.com>
2023-04-01 12:51:01 +0800

    添加书籍模糊搜索接口

--- src/main/java/com/example/readerserver/controller/BookController.java
+++ src/main/java/com/example/readerserver/controller/BookController.java
@@ -1,14 +1,18 @@
 package com.example.readerserver.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.readerserver.bean.Book;
 import com.example.readerserver.mapper.BookMapper;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
+@SuppressWarnings("all")// 清除警告
+@CrossOrigin(origins = {"*","null"})// 配置跨域
 public class BookController {
     private Gson gson = new Gson();
 
@@ -38,4 +42,14 @@ public class BookController {
         bookMapper.updateById(book);
         return gson.toJson("true");
     }
+    // 模糊搜索
+    @GetMapping("/book/search")
+    public String search(@RequestBody HashMap<String,String> data){
+        String name = data.get("name");
+        QueryWrapper<Book> bookQueryWrapper = new QueryWrapper<>();
+        bookQueryWrapper.like("name",name);
+        List<Book> books = bookMapper.selectList(bookQueryWrapper);
+        return gson.toJson(books) ;
+    }
+
 }
